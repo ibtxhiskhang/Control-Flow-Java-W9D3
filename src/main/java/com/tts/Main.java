339c63f81d94cc13cb9abbd6cff41e10a025315f
Ibtxhis Khang <79154883+ibtxhiskhang@users.noreply.github.com>
2021-04-14 16:47:16 -0400

    First Commit

---
+++ src/main/java/com/tts/Main.java
@@ -0,0 +1,60 @@
+package com.tts;
+import java.util.Locale;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello World");
+//
+//        AsciiChars.printNumbers();
+//        AsciiChars.printLowerCase();
+//        AsciiChars.printUpperCase();
+
+                UserInput.getInput();
+            }
+        }
+
+
+
+// using a switch-case way, but will have take extra work to create every question.
+//        String text = "Hello World";
+//        System.out.println(text);
+//        AsciiChars.printNumbers();
+//        AsciiChars.printLowerCase();
+//        AsciiChars.printUpperCase();
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please enter your name:");
+//        String yourName = scanner.nextLine();
+//        System.out.println("Hello " + yourName);
+////Must print "yes" or "Y" to continue the questions.
+//        System.out.println("Do you wish to continue? (y or n)");
+////        String promptAnswer = "y";
+//        String promptAnswer = scanner.nextLine().toLowerCase();
+//        switch (promptAnswer) {
+//            case "y":
+//            case "yes":
+//                System.out.println("Let's Continue!");
+//                System.out.println("Is your car red?");
+//                String promptAnswer2 = scanner.nextLine().toLowerCase();
+//                switch(promptAnswer2) {
+//                    case "y":
+//                    case "yes":
+//                    System.out.println("Thank you!");
+//                        break;
+//                    case "n":
+//                    case "no":
+//                    System.out.println("Goodbye!");
+//                        break;
+//                    default:
+//                    System.out.println("Please choose one");
+//                }
+//                break;
+//            case "n":
+//            case "no":
+//                System.out.println("Goodbye!");
+//                break;
+//            default:
+//                System.out.println("Please choose one");
+//        }
+//    }
+//}
